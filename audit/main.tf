65023006f15976c17499fae070f1b376bb00bcc4
tmknom <tmk.nom@gmail.com>
2018-11-24 20:41:47 +0900

    Implement S3 CloudTrail Bucket

--- audit/main.tf
+++ audit/main.tf
@@ -6,3 +6,11 @@ module "s3_access_log" {
 
   force_destroy = true
 }
+
+module "s3_cloudtrail" {
+  source                = "git::https://github.com/tmknom/terraform-aws-s3-cloudtrail.git?ref=tags/1.0.0"
+  name                  = "cloudtrail-${data.aws_caller_identity.current.account_id}"
+  logging_target_bucket = "${module.s3_access_log.s3_bucket_id}"
+
+  force_destroy = true
+}
--- audit/outputs.tf
+++ audit/outputs.tf
@@ -17,3 +17,23 @@ output "s3_access_log_bucket_hosted_zone_id" {
 output "s3_access_log_bucket_region" {
   value = "${module.s3_access_log.s3_bucket_region}"
 }
+
+output "s3_cloudtrail_bucket_id" {
+  value = "${module.s3_cloudtrail.s3_bucket_id}"
+}
+
+output "s3_cloudtrail_bucket_arn" {
+  value = "${module.s3_cloudtrail.s3_bucket_arn}"
+}
+
+output "s3_cloudtrail_bucket_domain_name" {
+  value = "${module.s3_cloudtrail.s3_bucket_domain_name}"
+}
+
+output "s3_cloudtrail_bucket_hosted_zone_id" {
+  value = "${module.s3_cloudtrail.s3_bucket_hosted_zone_id}"
+}
+
+output "s3_cloudtrail_bucket_region" {
+  value = "${module.s3_cloudtrail.s3_bucket_region}"
+}
