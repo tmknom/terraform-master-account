8e3fd660d8af68c7e9e41e7e59912021bf98a0b6
tmknom <tmk.nom@gmail.com>
2018-11-24 21:53:28 +0900

    Implement IAM Group of Admin

--- iam/main.tf
+++ iam/main.tf
@@ -1,5 +1,24 @@
 data "aws_caller_identity" "current" {}
 
+module "admin_iam_group" {
+  source = "git::https://github.com/tmknom/terraform-aws-iam-group.git?ref=tags/1.0.0"
+  name   = "Admin"
+  policy = "${data.aws_iam_policy_document.admin_iam_policy_document.json}"
+
+  users = []
+
+  path        = "/login/"
+  description = "Login administrators"
+}
+
+data "aws_iam_policy_document" "admin_iam_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
 module "iam_account" {
   source        = "git::https://github.com/tmknom/terraform-aws-iam-account.git?ref=tags/1.0.0"
   account_alias = "master-${data.aws_caller_identity.current.account_id}"
--- iam/outputs.tf
+++ iam/outputs.tf
@@ -1,3 +1,51 @@
+output "admin_iam_group_name" {
+  value = "${module.admin_iam_group.iam_group_name}"
+}
+
+output "admin_iam_group_id" {
+  value = "${module.admin_iam_group.iam_group_id}"
+}
+
+output "admin_iam_group_arn" {
+  value = "${module.admin_iam_group.iam_group_arn}"
+}
+
+output "admin_iam_group_path" {
+  value = "${module.admin_iam_group.iam_group_path}"
+}
+
+output "admin_iam_group_unique_id" {
+  value = "${module.admin_iam_group.iam_group_unique_id}"
+}
+
+output "admin_iam_group_membership_name" {
+  value = "${module.admin_iam_group.iam_group_membership_name}"
+}
+
+output "admin_iam_policy_id" {
+  value = "${module.admin_iam_group.iam_policy_id}"
+}
+
+output "admin_iam_policy_arn" {
+  value = "${module.admin_iam_group.iam_policy_arn}"
+}
+
+output "admin_iam_policy_description" {
+  value = "${module.admin_iam_group.iam_policy_description}"
+}
+
+output "admin_iam_policy_name" {
+  value = "${module.admin_iam_group.iam_policy_name}"
+}
+
+output "admin_iam_policy_path" {
+  value = "${module.admin_iam_group.iam_policy_path}"
+}
+
+output "admin_iam_policy_document" {
+  value = "${module.admin_iam_group.iam_policy_document}"
+}
+
 output "iam_account_password_policy_expire_passwords" {
   value = "${module.iam_account.iam_account_password_policy_expire_passwords}"
 }
